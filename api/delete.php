47fa6de1321289c46887384e96a24f4ae50f3ca1
yudayuda07 <kucingcool1@gmail.com>
2024-08-03 23:43:50 +0700

    Update Api

---
+++ api/delete.php
@@ -0,0 +1,20 @@
+<?php
+
+//render halaman menjadi json
+header('Content-Type: application/json');
+
+require '../config/app.php';
+
+parse_str(file_get_contents('php://input'), $delete);
+
+$id_barang = $delete['id_barang'];
+$query = "DELETE FROM barang where id_barang = $id_barang";
+mysqli_query($db,$query);
+ 
+if($query){
+    echo json_encode(['pesan' => 'Data Barang Berhasil Dihapus']);
+}else{
+    echo json_encode(['pesan' => 'Data Barang Gagal Dihapus']);
+}
+
+
